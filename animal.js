425598de2f13aec66a5fa718b9cf463efe45a456
Qlankz <zenquisaaron@gmail.com>
2023-11-09 23:27:34 -0600

    Switched to Nodejs

---
+++ animal.js
@@ -0,0 +1,78 @@
+class Animal {
+  constructor(name, hunger, thirst, energy, alive) {
+    this.name = name
+    this.hunger = hunger
+    this.thirst = thirst
+    this.energy = energy
+    this.alive = alive
+  }
+
+  static newRandomAnimal() {
+    const animalNames = [
+      'Lion',
+      'Elephant',
+      'Giraffe',
+      'Bunny',
+      'Tiger',
+      'Bear',
+    ]
+
+    const name = animalNames[Math.floor(Math.random() * animalNames.length)]
+    let hunger = Math.floor(Math.random() * 100)
+    let thirst = Math.floor(Math.random() * 100)
+    let energy = Math.floor(Math.random() * 100)
+
+    hunger = Math.max(50, hunger)
+    thirst = Math.max(50, thirst)
+    energy = Math.max(50, energy)
+
+    console.log(`A new ${name} was born into the lands`)
+    return new Animal(name, hunger, thirst, energy, true)
+  }
+
+  eat() {
+    if (this.hunger >= 100) {
+      this.hunger = 100
+    } else if (this.hunger <= 0) {
+      this.hunger = 0
+      this.alive = false
+      console.log(`A ${this.name} has died from hunger`)
+    } else {
+      console.log(`The ${this.name} is eating`)
+      this.hunger += 15
+      this.energy += 10
+    }
+  }
+
+  drink() {
+    if (this.thirst >= 100) {
+      this.thirst = 100
+    } else if (this.thirst <= 0) {
+      this.thirst = 0
+      this.alive = false
+      console.log(`A ${this.name} has died from thirst`)
+    } else {
+      console.log(`The ${this.name} is drinking`)
+      this.thirst += 25
+      this.energy += 5
+    }
+  }
+
+  sleep() {
+    if (this.energy >= 100) {
+      this.energy = 100
+    } else if (this.energy <= 0) {
+      this.energy = 10
+      console.log(`The ${this.name} has passed out from exhaustion`)
+    } else {
+      this.energy += 30
+    }
+  }
+
+  roaming() {
+    if (this.alive == true) {
+      console.log(`The ${this.name} is roaming the lands`)
+      this.energy -= 10
+    }
+  }
+}
--- main.js
+++ main.js
@@ -1,80 +1,24 @@
-class Animal {
-  constructor(name, hunger, thirst, energy, alive) {
-    this.name = name
-    this.hunger = hunger
-    this.thirst = thirst
-    this.energy = energy
-    this.alive = alive
-  }
+const animalMod = require('./animal')
+const readline = require('readline')
 
-  static newRandomAnimal() {
-    const animalNames = [
-      'Lion',
-      'Elephant',
-      'Giraffe',
-      'Bunny',
-      'Tiger',
-      'Bear',
-    ]
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-    const name = animalNames[Math.floor(Math.random() * animalNames.length)]
-    let hunger = Math.floor(Math.random() * 100)
-    let thirst = Math.floor(Math.random() * 100)
-    let energy = Math.floor(Math.random() * 100)
-
-    hunger = Math.max(50, hunger)
-    thirst = Math.max(50, thirst)
-    energy = Math.max(50, energy)
-
-    console.log(`A new ${name} was born into the lands`)
-    return new Animal(name, hunger, thirst, energy, true)
-  }
-
-  eat() {
-    if (this.hunger >= 100) {
-      this.hunger = 100
-    } else if (this.hunger <= 0) {
-      this.hunger = 0
-      this.alive = false
-      console.log(`A ${this.name} has died from hunger`)
-    } else {
-      console.log(`The ${this.name} is eating`)
-      this.hunger += 15
-      this.energy += 10
+function playGame() {
+  // Your game logic goes here
+  rl.question('Enter your command: ', (command) => {
+    // Process user input
+    if (command === 'quit') {
+      rl.close()
+      return
     }
-  }
 
-  drink() {
-    if (this.thirst >= 100) {
-      this.thirst = 100
-    } else if (this.thirst <= 0) {
-      this.thirst = 0
-      this.alive = false
-      console.log(`A ${this.name} has died from thirst`)
-    } else {
-      console.log(`The ${this.name} is drinking`)
-      this.thirst += 25
-      this.energy += 5
-    }
-  }
-
-  sleep() {
-    if (this.energy >= 100) {
-      this.energy = 100
-    } else if (this.energy <= 0) {
-      this.energy = 10
-      console.log(`The ${this.name} has passed out from exhaustion`)
-    } else {
-      this.energy += 30
-    }
-  }
-
-  roaming() {
-    if (this.alive == true) {
-      console.log(`The ${this.name} is roaming the lands`)
-      this.energy -= 10
-    }
-  }
+    // Continue the game or handle user commands
+    // Example: console.log(`You entered: ${command}`);
+    playGame()
+  })
 }
 
-
+playGame()
